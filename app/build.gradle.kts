5fce2c8da9842fb4eb2849bafedf9da867a6f891
pascal <pascaladitia@gmail.com>
2024-11-12 14:53:15 +0700

    - add font & drawable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.pascal.myapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.pascal.myapplication"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
